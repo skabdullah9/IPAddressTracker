2af86fee72696868fc78feedfb26cc4b82aad4e9
skabdullah9 <skabdullah9941@gmail.com>
2021-09-02 23:16:53 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="min-h-screen h-screen max-h-screen flex flex-col">
+    <header class="flex flex-col pt-2 md:pt-4 pb-12 md:pb-24 px-2 relative">
+        <h1 class="font-mono text-3xl mb-2 md:mb-5 text-gray-200">IP Address Tracker</h1>
+      <form @submit.prevent="getIpInfo" class="search flex max-w-xl w-full mx-auto font-mono ">
+        <input v-model="queryIp" placeholder="Search any IP address or leave it blank to fetch current IP" type="text" class="w-full text-base md:text-2xl py-1 px-3 focus:outline-none rounded-tl-md rounded-bl-md" />
+        <button type="submit" @click="getIpInfo" class="bg-black p-2 h-full rounded-tr-md rounded-br-md"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+</svg></button>
+      </form>
+      <Ipinfo v-if="ipData.value" :ipData="ipData.value" />
+    </header>
+    <div id="map" class="h-full z-10"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ipinfo from "./components/Ipinfo.vue";
+import leaflet from "leaflet";
+import { onMounted, ref, reactive } from "@vue/runtime-core";
+import axios from 'axios'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Ipinfo,
+  },
+  setup() {
+    let map;
+    const queryIp = ref('')
+    const ipData = reactive({})
+    
+    onMounted(() => {
+      map = leaflet.map("map").setView([51.505, -0.09], 9);
+      
+      leaflet
+        .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        })
+        .addTo(map);
+    });
+    
+    const getIpInfo = async () => {
+      try{
+        const response = await axios(`https://geo.ipify.org/api/v1?apiKey=at_BtlDH0GZhLrj3qwiPgRyGlOyS0898&ipAddress=${queryIp.value}`)
+        const data = response.data
+       
+        ipData.value = {
+          ip: data.ip,
+          isp: data.isp,
+          timezone: data.location.timezone,
+          location: data.location.region,
+          lat: data.location.lat,
+          lng: data.location.lng,
+        }
+        const {lat, lng} = ipData.value
+       
+       
+        map.flyTo([lat, lng], 13, {duration:5});
+        leaflet.marker([lat, lng]).addTo(map)
+        
+        
+
+        setTimeout(() => {
+          leaflet.marker([lat, lng]).addTo(map)
+          .bindPopup(`${data.location.city}, ${data.location.country}`, {closeButton: false})
+          .openPopup()
+        }, 4500);
+
+        
+      }
+      catch(err) {
+        ipData.value = {}
+        
+      }
+    }
+    return {queryIp, ipData, getIpInfo, }
+  },
+  
+};
 </script>
 
 <style>
@@ -21,6 +88,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+header {
+  background-color: #3c40c6;
+
+background-image: url("https://www.transparenttextures.com/patterns/inspiration-geometry.png");
+ 
+}
+.leaflet-popup-content-wrapper {
+  margin-left: -10px;
+}
+.leaflet-popup-content {
+ font-size: 1.2rem;
+ margin: 0.5rem;
+ white-space: nowrap;
+ width: auto !important;
+font-weight: 500;
+}
+.leaflet-marker-icon {
+  filter: hue-rotate(30deg) saturate(100%) contrast(200%) !important
+}
+.leaflet-control-zoom {
+  margin-top: 50px !important;
 }
 </style>
---
+++ src/components/Ipinfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-white max-w-xl w-11/12 h-14 absolute top-3/4 md:top-auto md:bottom-0 left-1/2 transform -translate-x-1/2 md:translate-y-1/2 z-20 rounded-md overflow-hidden md:h-auto shadow-2xl"
+  :class="{'h-auto': showInfo}">
+   
+      
+      <div v-if="ipData.ip" class="flex flex-wrap flex-col md:flex-row text-gray-900 font-sans px-5 py-2 md:py-4 text-left justify-between items-start gap-1 md:gap-2">
+        <div >
+          <p class="uppercase text-xs md:text-sm font-mono ">ip address</p>
+          <h2 class="font-sans font-semibold text-base md:text-lg">{{ipData.ip}}</h2>
+        </div>
+        <div >
+          <p class="uppercase text-xs md:text-sm font-mono">location</p>
+          <h2 class="font-sans font-semibold text-base md:text-lg capitalize">{{ipData.location}}</h2>
+        </div>
+        <div >
+          <p class="uppercase text-xs md:text-sm font-mono">time zone</p>
+          <h2 class="font-sans font-semibold text-base md:text-lg uppercase">utc {{ipData.timezone}}</h2>
+        </div>
+        <div >
+          <p class="uppercase text-xs md:text-sm font-mono">isp</p>
+          <h2 class="font-sans font-semibold text-base md:text-lg capitalize">{{ipData.isp}}</h2>
+        </div>
+      </div>
+      <div v-else class="flex items-center justify-center h-20 font-semibold text-base md:text-lg font-sans">
+        <h1>Invalid IP Address.</h1>
+      </div>
+       <button v-show="ipData.ip" @click="showInfo = !showInfo" class="absolute right-3 top-2 p-2 rounded-tr-md rounded-br-md  md:hidden"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 transform rotate-90 transition-transform" :class="{'-rotate-90' : showInfo}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+</svg></button>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+  props: {'ipData' : Object},
+  setup() {
+    const showInfo = ref(false)
+
+    return {showInfo}
+  }
+}
+</script>
+
+<style>
+
+</style>
